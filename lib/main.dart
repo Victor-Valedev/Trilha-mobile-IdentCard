d6126e625e085f00e6529ec763c21173dedfa2b1
Victor-Valedev <victorvaledev@gmail.com>
2025-05-05 09:32:22 -0300

    feat: cria tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,18 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Identification card',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xFF0F2027),
+          primary: Color(0xFF203A43),
+          secondary: Color(0xFF2C5364),
+        ),
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
       ),
       home: LoginPage(),
     );
--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -11,10 +11,72 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Identification card'),
+      appBar: AppBar(title: Text('Identification card')),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF0F2027), Color(0xFF203A43), Color(0xFF2C5364)],
+          ),
+        ),
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Seja bem-vindo(a)!',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Faça login para continuar',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Username: ',
+                    prefixIcon: Icon(Icons.email),
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Password: ",
+                    prefixIcon: Icon(Icons.password),
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(onPressed: () {}, child: Text('Login')),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  textStyle: TextStyle(decoration: TextDecoration.underline),
+                ),
+                child: Text(
+                  'Esqueci minha senha',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
