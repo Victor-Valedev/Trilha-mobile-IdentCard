cbf8a38a9bb3a147fea3ebb64ba428f81269e13b
Victor-Valedev <victorvaledev@gmail.com>
2025-05-05 11:10:13 -0300

    feat: cria função para acessar dados da API

---
+++ lib/service/client_http.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
+import 'package:trilhamobileatvd/model/dto/user_request_dto.dart';
+import 'package:trilhamobileatvd/model/dto/user_response_dto.dart';
+import 'package:http/http.dart' as http;
+import 'package:trilhamobileatvd/utils/constants.dart';
+
+class ClientHttp {
+  Future<UserResponseDto> login(UserRequestDto user) async {
+    final response = await http.post(
+      Uri.parse(Constants.BASE_URL),
+      headers: {'Content-Type': 'application/json'},
+      body: user.toJson(),
+    );
+
+    if (response.statusCode == 200) {
+      UserRequestDto.fromJson(response.body);
+    }
+
+    if (response.statusCode == 400) {
+      final message = jsonDecode(response.body);
+      throw FlutterError(message['message']);
+    }
+
+    throw FlutterError('Falha ao realizar login');
+  }
+}
